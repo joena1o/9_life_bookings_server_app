82d501dc51f28e68631bcbed999423fcea5dca00
Hyefur Jonathan Balami <jonathanhyefur@gmail.com>
2025-02-14 15:17:00 +0100

    notification model added to rating and paystack webhook

---
+++ src/controllers/notification.controller.ts
@@ -0,0 +1,19 @@
+import notificationModel from "../models/notification_model";
+import {Response, Request} from 'express';
+import { decodeToken } from "../utils/jwt_service";
+import { JwtPayload } from "jsonwebtoken";
+
+
+export const getNotifications = async(req: Request, res: Response): Promise<any> =>{
+    try{
+        const accessToken = decodeToken(req.headers.authorization!);
+        if (accessToken && typeof accessToken !== "string" && accessToken.payload) {
+            const payload = accessToken.payload as JwtPayload; // Cast to JwtPayload
+            const user_id = payload.userId;
+        const getNotifications = await notificationModel.find({user_id: user_id});
+        return res.status(200).json({data: getNotifications});
+    }
+    }catch(error){
+        return res.status(500).json({error: error});
+    }
+}
---
+++ src/models/notification_model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema({
+  user_id: {
+    type: String,
+    required: true,
+  },
+  product_id: {
+    type: String,
+    ref: "product",
+    default: null
+  },
+  noticeType: {
+    type: String,
+    enum: ['notice','purchase','sale','review'],
+    default: "notice"
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  extra: {
+    type: String,
+    default: null
+  }
+}, {timestamps: true});
+
+const notificationModel = mongoose.model("notification", notificationSchema);
+export default notificationModel;
