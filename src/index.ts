a0dd1a245d7d1a31bd7a5d44572fb1dd88052b69
joena1o <jonathanhyefur@gmail.com>
2025-07-05 10:46:01 +0100

    email request changed

--- src/index.ts
+++ src/index.ts
@@ -192,5 +192,5 @@ app.get('/', async (req: Request, res: Response) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT} ${process.env.EMAIL_SERVER_API}`);
 });
